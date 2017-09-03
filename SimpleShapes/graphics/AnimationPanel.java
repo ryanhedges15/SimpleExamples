56508d0b0724c214659b63bb606c4e6c20735c2d
Ryan Hedges <ryanhedges15@gmail.com>
2017-09-02 20:09:09 -0400

    Added a 2D Graphics Example

--- SimpleShapes/graphics/AnimationPanel.java
+++ SimpleShapes/graphics/AnimationPanel.java
@@ -15,31 +15,37 @@ public class AnimationPanel extends JPanel implements ActionListener {
 
     public AnimationPanel() {
         this.setBackground(Color.white);
-        Timer timer = new Timer(500,this);
+        Timer timer = new Timer(500, this);
         timer.start();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        Graphics2D graphics2D = (Graphics2D)g.create();
+        Graphics2D graphics2D = (Graphics2D) g.create();
+
+        int currFrame = frameNumber % 4;
+        if (currFrame >= 0) {
+            graphics2D.translate(5, 7);
+        }
+        if (currFrame >= 1) {
+            graphics2D.rotate(-1 * 60 * Math.PI / 180.0);
+        }
+        if (currFrame >= 2) {
+            graphics2D.rotate(90 * Math.PI / 180.0);
+        }
+        if (currFrame >= 3) {
+            graphics2D.scale(2, .5);
+        }
+
         graphics2D.clearRect(0, 0, this.getWidth(), this.getHeight());
         graphics2D.setPaint(Color.WHITE);
-        graphics2D.fillRect(0,0,this.getWidth(),this.getHeight());
-        graphics2D.scale(4d,4d);
-        graphics2D.drawImage(new QuestionCube().asImage(),100,75,this);
+        graphics2D.fillRect(0, 0, this.getWidth(), this.getHeight());
+        graphics2D.drawImage(new QuestionCube().asImage(), 300, 250, this);
 
-        if(frameNumber%8==1) {
-            System.out.println("with coins");
-            graphics2D.scale(.5, .5);
-            graphics2D.drawImage(new Coin().asImage(), 110 * 2, 50 * 2, this);
-            graphics2D.drawImage(new Coin().asImage(), 110 * 2, 30 * 2, this);
-        }
-        if(frameNumber%8==4) {
-            System.out.println("with coins");
-            graphics2D.scale(.5, .5);
-            graphics2D.drawImage(new Coin().asImage(), 110 * 2, 30 * 2, this);
-            graphics2D.drawImage(new Coin().asImage(), 110 * 2, 10 * 2, this);
-        }
+        graphics2D.setPaint(Color.BLACK);
+        graphics2D.drawImage(new Coin().asImage(), 110 * 2, 50 * 2, this);
+        graphics2D.drawImage(new Coin().asImage(), 110 * 2, 35 * 2, this);
+
     }
 
     public void actionPerformed(ActionEvent event) {
